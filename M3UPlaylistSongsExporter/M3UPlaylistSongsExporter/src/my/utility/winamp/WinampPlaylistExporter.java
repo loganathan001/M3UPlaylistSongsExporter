e0de3f4b99948c457ae69e17a06e7f5e9bf5b13e
Loganathan S <loganathans@ilink-systems.com>
2016-10-13 14:31:23 +0530

    Using Files.lines to read lines of m3u.

--- M3UPlaylistSongsExporter/M3UPlaylistSongsExporter/src/my/utility/winamp/WinampPlaylistExporter.java
+++ M3UPlaylistSongsExporter/M3UPlaylistSongsExporter/src/my/utility/winamp/WinampPlaylistExporter.java
@@ -2,15 +2,11 @@ package my.utility.winamp;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -56,59 +52,37 @@ public class WinampPlaylistExporter {
 			return;
 		}
 
-		String readFile = readFile(inputFile.toString());
-		List<String> lines = Arrays.stream(readFile.split("\n"))
-				.map(line -> line.trim()).collect(Collectors.toList());
-		Stream<String> filteredListStream = lines.stream().filter(
-				l -> !l.startsWith("#"));
-		List<String> list = filteredListStream.collect(Collectors.toList());
-		totalFilesCount = list.size();
-		monitorCount = 0;
-		// monitor.done();
-		monitor.beginTask("Exporting songs:", (int) totalFilesCount);
-
-		Stream<String> listStream = list.stream();
-		listStream.onClose(new Runnable() {
-			@Override
-			public void run() {
-				if (!monitor.isCanceled()) {
-					Display.getDefault().syncExec(new Runnable() {
-						@Override
-						public void run() {
-							monitor.done();
-						}
-					});
-					System.out.println("Export completed.");
-				} else {
-					System.out.println("Cancel Requested.");
-				}
-			}
-		});
-		listStream.forEach(this::doExportFile);
-
-	}
-
-	private String readFile(String filePath) {
-		StringBuffer stringBuffer = new StringBuffer();
 		try {
-			RandomAccessFile aFile = new RandomAccessFile(filePath, "r");
-			FileChannel inChannel = aFile.getChannel();
-			MappedByteBuffer buffer = inChannel.map(
-					FileChannel.MapMode.READ_ONLY, 0, inChannel.size());
-			buffer.load();
-			for (int i = 0; i < buffer.limit(); i++) {
-				stringBuffer.append((char) buffer.get());
-			}
-			buffer.clear(); // do something with the data and clear/compact it.
-			inChannel.close();
-			aFile.close();
+			Stream<String> filteredLineStream = Files.lines(Paths.get(new File(inputFile.toString()).toURI())).map(line -> line.trim()).filter(
+					line -> !line.startsWith("#"));
+			 List<String> linesList = filteredLineStream.collect(Collectors.toList());
+			totalFilesCount = linesList.size();
+			monitorCount = 0;
+			// monitor.done();
+			monitor.beginTask("Exporting songs:", (int) totalFilesCount);
 
+			linesList.stream().onClose(new Runnable() {
+				@Override
+				public void run() {
+					if (!monitor.isCanceled()) {
+						Display.getDefault().syncExec(new Runnable() {
+							@Override
+							public void run() {
+								monitor.done();
+							}
+						});
+						System.out.println("Export completed.");
+					} else {
+						System.out.println("Cancel Requested.");
+					}
+				}
+			}).forEach(this::doExportFile);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return stringBuffer.toString();
+
 	}
+
 
 	private void doExportFile(String filePath) {
 		++monitorCount;
@@ -141,7 +115,6 @@ public class WinampPlaylistExporter {
 					}
 				});
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else {
